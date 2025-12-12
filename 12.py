3f04bda466713a11846c8c243bbcd808d14862b7
Rishita510897 <rishithapola567@gmail.com>
2025-12-12 15:18:24 +0000

    Added msg

---
+++ 12.py
@@ -0,0 +1,91 @@
+#1
+print('Hello SkillGapAI') # Hello SkillGapAI
+#2
+name='Rishita' 
+print(name) # Rishita
+# 3
+age=25
+city='Chennai'
+skill='Python'
+print(age,city,skill) # 25 Chennai Python
+# 4
+val=str(100)
+print(type(val)) # <class 'str'>
+# 5
+text='python basics'
+print(text.upper()) # PYTHON BASICS
+# 6
+print(len("SkillGapAI")) # 10
+# 7
+colors=["red","blue","green"]
+print(colors[1])   # blue
+# 8
+fruits=['apple','banana']
+fruits.append('orange')
+print(fruits) # ['apple', 'banana', 'orange']
+# 9
+items=['apple','banana','orange']
+items.remove('banana')
+print(items) # ['apple', 'orange']
+# 10
+nums=[1,2,3]
+t=tuple(nums)
+print(t) # (1, 2, 3)
+# 11
+resume={'name':'John','age':25,'skills':['Python','SQL']}
+print(resume) # {'name': 'John', 'age': 25, 'skills': ['Python', 'SQL']}
+# 12
+print(resume['skills']) # ['Python', 'SQL']
+# 13
+marks=50
+print('Pass' if marks>40 else 'Fail') # Pass
+# 14
+num=7
+print('Even' if num%2==0 else 'Odd') # Odd
+# 15
+for i in range(1,11):
+    print(i)
+# Output
+"""
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+"""
+#16
+j=5
+while j>=1:
+    print(j)
+    j-=1
+# Output
+"""
+5
+4
+3
+2
+1
+"""
+# 17
+def say_hello():
+    print('Hello')
+say_hello() # Hello
+# 18
+def odd(a,b):
+    return a+b
+print(odd(12,34)) # 46
+# 19
+import math 
+print(math.pi) # 3.141592653589793
+# 20 
+
+
+
+
+
+
